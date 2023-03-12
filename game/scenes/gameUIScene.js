9550d353fb80bb170e7fe0fcb3ae03f50c83a0a7
ilyamed <0ilya@bk.ru>
2023-03-12 18:28:25 +0300

    set score text to white and move down

--- game/scenes/gameUIScene.js
+++ game/scenes/gameUIScene.js
@@ -7,8 +7,7 @@ export class GameUIScene extends Phaser.Scene {
 
 	create() {
 		//  Our Text object to display the Score
-		var info = this.add.text(10, 10, 'Score: 0', { font: '48px Arial', fill: '#000000', color: 'white' })
-
+		var info = this.add.text(10, 70, 'Score: 0', { font: '48px Arial', fill: '#FFFFFF' })
 		//  Grab a reference to the Game Scene
 		var gameScene = this.scene.get('GameScene')
 
